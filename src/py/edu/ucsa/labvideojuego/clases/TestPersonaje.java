059b6ec45b96b0b0827b4e6ec780872b60620e8c
carmen-mendieta <carmenmendieta029@gmail.com>
2023-06-20 11:18:38 -0400

    Test sysout

--- src/py/edu/ucsa/labvideojuego/clases/TestPersonaje.java
+++ src/py/edu/ucsa/labvideojuego/clases/TestPersonaje.java
@@ -2,6 +2,7 @@ package py.edu.ucsa.labvideojuego.clases;
 
 public class TestPersonaje {
 	public static void main(String[] args) {
+		System.out.println(" ");
 
 		ManejadordePersonajes manejador = new ManejadordePersonajes();
 		manejador.generarPersonajes();
